35bc77e82d0f10718e66d4ae01fb91b6e9b2388d
btrshn <benguhansahin@gmail.com>
2024-02-28 14:56:49 +0300

    create counter

---
+++ counterProgram/index.js
@@ -0,0 +1,23 @@
+//Counter Program
+
+const decrementButton = document.getElementById('decrementButton');
+const resetButton = document.getElementById('resetButton');
+const incrementButton = document.getElementById('incrementButton');
+const countLabel = document.getElementById('countLabel');
+
+let count = 0;
+
+incrementButton.onclick = function() {
+  count++;
+  countLabel.textContent = count;
+}
+
+decrementButton.onclick =function() {
+    count--;
+    countLabel.textContent = count;
+}
+
+resetButton.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
